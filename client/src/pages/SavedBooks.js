f68d92143a81580c46ffb51c9eaf7962f40bed44
BooMajka <visionseers@gmail.com>
2022-04-02 11:41:20 -0500

    save book logic

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -1,14 +1,22 @@
 import React, { useState, useEffect } from 'react';
 import { Jumbotron, Container, CardColumns, Card, Button } from 'react-bootstrap';
 
-import { getMe, deleteBook } from '../utils/API';
+// import { getMe, deleteBook } from '../utils/API';
 import Auth from '../utils/auth';
 import { removeBookId } from '../utils/localStorage';
+import { useMutation, useQuery } from "@apollo/client";
+import { GET_ME } from "../utils/queries";
+import { REMOVE_BOOK } from "../utils/mutation";
 
 const SavedBooks = () => {
+  const { loading, data } = useQuery(GET_ME);
+  const [removeBook] = useMutation(REMOVE_BOOK);
   const [userData, setUserData] = useState({});
 
-  // use this to determine if `useEffect()` hook needs to run again
+  if (loading) {
+    return <h2>LOADING...</h2>;
+  }
+  // use this to determine if `useEffect()` hook needs to run again   ????
   const userDataLength = Object.keys(userData).length;
 
   useEffect(() => {
